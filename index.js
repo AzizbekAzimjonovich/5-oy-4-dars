db052360c24748e1a9449273806752514aff566c
AzizbekAzimjonovich <azizbekazimjonovich@gmail.com>
2024-01-18 18:23:33 +0500

    vazifa

--- index.js
+++ index.js
@@ -14,19 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 <button type="button" class="btn btn-outline-danger">delete</button>
             `;
             list.appendChild(li);
-
-            // Clear the input field after saving
             input.value = '';
-
-            // Attach the delete button event listener to the new delete button
             const deleteButton = li.querySelector('.btn-outline-danger');
             deleteButton.addEventListener('click', function () {
                 list.removeChild(li);
             });
         }
     });
-
-    // Add event listeners for existing delete buttons
     const deleteButtons = document.querySelectorAll('.btn-outline-danger');
     deleteButtons.forEach(function (deleteButton) {
         deleteButton.addEventListener('click', function () {
